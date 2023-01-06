ebb5471dc7b0a8a776df74dec950541d1277f113
BerkayCimendag <cimendagberkay@gmail.com>
2023-01-06 17:37:53 +0300

    admin kategoriler gonderiler

---
+++ BlogSite_MVC/Areas/Admin/Controllers/GonderilerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GonderilerController : Controller
+    {
+        private readonly UygulamaDbContext _db;
+
+        public GonderilerController(UygulamaDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Gonderiler.Include(x=>x.Kategori).ToList());
+        }
+
+
+    }
+}
