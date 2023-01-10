41ca91f09fcbb3f68e182e9dcc5fc22fbf677fef
BerkayCimendag <cimendagberkay@gmail.com>
2023-01-10 16:02:13 +0300

    gonderi duzenle ve sil

--- BlogSite_MVC/Areas/Admin/Controllers/GonderilerController.cs
+++ BlogSite_MVC/Areas/Admin/Controllers/GonderilerController.cs
@@ -39,7 +39,6 @@ namespace BlogSite_MVC.Areas.Admin.Controllers
                     Baslik = vm.Baslik,
                     Icerik = vm.Icerik,
                     KategoriId = vm.KategoriId!.Value
-                
                 };
                 _db.Gonderiler.Add(gonderi);
                 _db.SaveChanges();
@@ -50,9 +49,58 @@ namespace BlogSite_MVC.Areas.Admin.Controllers
         }
         public IActionResult Duzenle(int id)
         {
+            var gonderi = _db.Gonderiler.Find(id);
+
+            if (gonderi == null)
+            {
+                return NotFound();
+            }
+            var vm = new GonderiViewModel()
+            {
+                Id=gonderi.Id,
+                Baslik = gonderi.Baslik,
+                Icerik= gonderi.Icerik,
+                KategoriId= gonderi.KategoriId
+            };
+            KategorileriYukle();
+            return View("Yonet",vm);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Duzenle(GonderiViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var gonderi = _db.Gonderiler.Find(vm.Id);
+                if (gonderi == null) 
+                {
+                    return NotFound();
+                }
+                gonderi.Baslik = vm.Baslik;
+                gonderi.Icerik = vm.Icerik;
+                gonderi.DegistirilmeZamani = DateTime.Now;
+                gonderi.KategoriId = vm.KategoriId!.Value;
+                
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index), new { durum = "duzenlendi" });
+            }
+            KategorileriYukle();
             return View("Yonet");
         }
 
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult Sil(int id)
+        {
+            var gonderi = _db.Gonderiler.Find(id);
+
+            if (gonderi == null)
+            {
+                return NotFound();
+            }
+
+            _db.Gonderiler.Remove(gonderi);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index), new { durum = "silindi" });
+        }
 
     }
 }
